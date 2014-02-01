4897ee061a2cd7ecb53ac77e0f8651fd9470fcfe
Anna Voelkl <anna@vape.net>
2014-02-01 01:45:43 +0100

    Added Flag to Admin User form to turn TwoFactorAuth on/off

---
+++ src/app/code/community/MageHackDay/TwoFactorAuth/Block/Adminhtml/Permissions/User/Edit/Tab/Main.php
@@ -0,0 +1,35 @@
+<?php
+
+class MageHackDay_TwoFactorAuth_Block_Adminhtml_Permissions_User_Edit_Tab_Main extends Mage_Adminhtml_Block_Permissions_User_Edit_Tab_Main
+{
+	protected function _prepareForm()
+    {
+		parent::_prepareForm();
+		
+		$form = $this->getForm();
+		
+		$fieldset = $form->addFieldset('ga2fa_fieldset', array('legend'=>$this->__('Two Factor Authentication')));
+		
+		$fieldset->addField('twofactorauth', 'select', array(
+          'label'     => $this->__('Use Google Authenticator'),
+          'class'     => 'required-entry',
+          'required'  => true,
+          'name'      => 'twofactorauth',
+          'onclick' => "",
+          'onchange' => "",
+          'value'  => '1',
+          'values' => array('0' => 'No','1' => 'Yes'),
+          'disabled' => false,
+          'readonly' => false,
+          'after_element_html' => '<small>When using the Two Factor Authentication, a 6-digit code needs to be entered in addition to username and password during login. Two Factor Authentication Google provides the Authenticator app for Android, BlackBerry and iOS.</small>',
+        ));
+		
+		$model = Mage::registry('permissions_user');
+		$data = $model->getData();
+        unset($data['password']);
+        $form->setValues($data);
+
+        return $this;
+	}
+}
+	
