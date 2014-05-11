bd37cfc329053130201814b3529c396b31f66dd0
Dan Fugina <dan@ablecanyon.com>
2014-05-10 18:05:38 -0700

    First rough pass at configuring 2fa on account page

---
+++ src/app/code/community/MageHackDay/TwoFactorAuth/Block/Adminhtml/System/Account/Edit/Form.php
@@ -0,0 +1,58 @@
+<?php
+
+require_once (Mage::getBaseDir('lib') . DS . 'GoogleAuthenticator' . DS . 'PHPGangsta' . DS . 'GoogleAuthenticator.php');
+
+class MageHackDay_TwoFactorAuth_Block_Adminhtml_System_Account_Edit_Form
+    extends Mage_Adminhtml_Block_System_Account_Edit_Form
+{
+    protected function _prepareForm()
+    {
+        parent::_prepareForm();
+
+        $userId = Mage::getSingleton('admin/session')->getUser()->getId();
+        $user = Mage::getModel('admin/user')
+            ->load($userId);
+
+        // Create a new secret each page load, even if it's not being used
+        $ga = new PHPGangsta_GoogleAuthenticator();
+        $secret = $ga->createSecret();
+        $qrCodeUrl = $ga->getQRCodeGoogleUrl('Magento', $secret);
+
+        $form = $this->getForm();
+
+        $fieldset = $form->addFieldset(
+            'twofactorauth',
+            array('legend' => Mage::helper('adminhtml')->__('Two Factor Authentication'))
+        );
+
+        $fieldset->addField('twofactorauth_secret', 'hidden', array(
+            'name' => 'twofactorauth_secret',
+            'value' => $secret
+        ));
+
+        $fieldset->addField('twofactorauth_configured', 'label', array(
+                'name'  => 'twofactorauth_configured',
+                'label' => Mage::helper('twofactorauth')->__('Configured'),
+                'title' => Mage::helper('twofactorauth')->__('Configured'),
+                'value' => ($user->getTwofactorToken()) ? Mage::helper('twofactorauth')->__('Yes') : Mage::helper('twofactorauth')->__('No')
+            )
+        );
+
+        $fieldset->addField('twofactor_token', 'label', array(
+                'name'  => 'twofactor_token',
+                'label' => Mage::helper('twofactorauth')->__('Secret Key'),
+                'title' => Mage::helper('twofactorauth')->__('Secret Key'),
+                'after_element_html' => "<img src=\"$qrCodeUrl\" />"
+            )
+        );
+
+        $fieldset->addField('twofactorauth_code', 'text', array(
+                'name'  => 'twofactorauth_code',
+                'label' => Mage::helper('twofactorauth')->__('Code'),
+                'title' => Mage::helper('twofactorauth')->__('Code')
+            )
+        );
+
+        $this->setForm($form);
+    }
+}
