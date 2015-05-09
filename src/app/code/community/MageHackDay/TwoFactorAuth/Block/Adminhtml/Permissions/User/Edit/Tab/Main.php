b63d34ef7172f224ffc7d082d02f9717d0e3905a
Gerhard Fobe <gerhard.fobe@nr-apps.com>
2015-05-09 15:03:12 +0200

    code style: replace tabs with spaces

--- src/app/code/community/MageHackDay/TwoFactorAuth/Block/Adminhtml/Permissions/User/Edit/Tab/Main.php
+++ src/app/code/community/MageHackDay/TwoFactorAuth/Block/Adminhtml/Permissions/User/Edit/Tab/Main.php
@@ -2,15 +2,15 @@
 
 class MageHackDay_TwoFactorAuth_Block_Adminhtml_Permissions_User_Edit_Tab_Main extends Mage_Adminhtml_Block_Permissions_User_Edit_Tab_Main
 {
-	protected function _prepareForm()
+    protected function _prepareForm()
     {
-		parent::_prepareForm();
-		
-		$form = $this->getForm();
-		
-		$fieldset = $form->addFieldset('ga2fa_fieldset', array('legend'=>$this->__('Two Factor Authentication')));
-		
-		$fieldset->addField('twofactorauth', 'select', array(
+        parent::_prepareForm();
+
+        $form = $this->getForm();
+
+        $fieldset = $form->addFieldset('ga2fa_fieldset', array('legend'=>$this->__('Two Factor Authentication')));
+
+        $fieldset->addField('twofactorauth', 'select', array(
           'label'     => Mage::helper('twofactorauth')->__('Use Google Authenticator'),
           'class'     => 'required-entry',
           'required'  => true,
@@ -23,13 +23,12 @@ class MageHackDay_TwoFactorAuth_Block_Adminhtml_Permissions_User_Edit_Tab_Main e
           'readonly' => false,
           'after_element_html' => '<small>When using the Two Factor Authentication, a 6-digit code needs to be entered in addition to username and password during login. Two Factor Authentication Google provides the Authenticator app for Android, BlackBerry and iOS.</small>',
         ));
-		
-		$model = Mage::registry('permissions_user');
-		$data = $model->getData();
+
+        $model = Mage::registry('permissions_user');
+        $data = $model->getData();
         unset($data['password']);
         $form->setValues($data);
 
         return $this;
-	}
+    }
 }
-	
